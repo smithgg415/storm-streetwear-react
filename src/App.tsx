83bd4e9d481bbbac702f19861e7888393b06823f
Giacomelli <133823261+smithgg415@users.noreply.github.com>
2025-03-30 05:24:51 +0000

    [fix] Remove routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import About from "./pages/About";
 import NotFound from "./pages/NotFound";
-import ProductDetail from "./pages/ProductDetail";
 
 const queryClient = new QueryClient();
 
