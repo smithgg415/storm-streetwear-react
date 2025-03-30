149101bcf5d968ab9784c77c78a140cb6b881f2c
Giacomelli <133823261+smithgg415@users.noreply.github.com>
2025-03-30 06:26:53 +0000

    [fix] Revendedores

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import Revendedores from "./pages/revendedores/index";
+import Revendedores from "./pages/Revendedores";
 const queryClient = new QueryClient();
 
 const App = () => (
